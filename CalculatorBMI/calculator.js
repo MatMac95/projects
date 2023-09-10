5b790c90beec50a5e73932dea7003441bde93401
Mateusz Maciejewski <mat.maciejewski95@gmail.com>
2023-09-10 18:47:58 +0200

    BMI calc update

--- CalculatorBMI/calculator.js
+++ CalculatorBMI/calculator.js
@@ -2,34 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.post("/", (req, res) => {
-    res.send("Thanks for posting");
-
+  res.send("Thanks for posting");
 });
 
 app.get("/bmicalculator", (req, res) => {
-    
-    res.sendFile(__dirname + "/bmiCalculator.html");
-  
+  res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.post("/bmicalculator", (req, res) => {
-    
-    const weight = req.body.weight;
-    const heightInCentymeters = req.body.height;
-    const heightInMeters = heightInCentymeters/100;
-    const BMI = weight/(heightInMeters*heightInMeters);
-    res.send("Twój wynik to: " + BMI.toFixed(1));
+  const weight = req.body.weight;
+  const heightInCentymeters = req.body.height;
+  const heightInMeters = heightInCentymeters / 100;
+  const BMI = weight / (heightInMeters * heightInMeters);
+  res.send("Twój wynik to: " + BMI.toFixed(1));
 });
 
-app.listen(port, ()=> {
-    console.log(`Server is listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
