e92f2e8294b2fa037d5e9f82b3994eef207a38e9
Mateusz Maciejewski <mat.maciejewski95@gmail.com>
2023-09-10 18:44:58 +0200

    BMI calc

---
+++ CalculatorBMI/calculator.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    res.send("Thanks for posting");
+
+});
+
+app.get("/bmicalculator", (req, res) => {
+    
+    res.sendFile(__dirname + "/bmiCalculator.html");
+  
+});
+
+app.post("/bmicalculator", (req, res) => {
+    
+    const weight = req.body.weight;
+    const heightInCentymeters = req.body.height;
+    const heightInMeters = heightInCentymeters/100;
+    const BMI = weight/(heightInMeters*heightInMeters);
+    res.send("Twój wynik to: " + BMI.toFixed(1));
+});
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on port ${port}`)
+});
